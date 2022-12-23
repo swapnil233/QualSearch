59111a0989e8a44e9273289740123013d4b4bb70
Hasan Iqbal <swapniliqbal@gmail.com>
2022-12-23 17:52:42 -0500

    Updated middlewares

--- server/index.js
+++ server/index.js
@@ -1,1 +1,35 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const morgan_1 = __importDefault(require("morgan"));
+const body_parser_1 = __importDefault(require("body-parser"));
+// import cors from 'cors';
+const helmet_1 = __importDefault(require("helmet"));
+// Configurations and middlewares
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(helmet_1.default);
+app.use(helmet_1.default.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use((0, morgan_1.default)('common'));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+// app.use(cors());
+const PORT = process.env.PORT || 3000;
+// 404 Page
+app.get("*", (req, res) => {
+    res.status(404).json({ message: "Page not found" });
+});
+// Server
+try {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+catch (error) {
+    console.error(error);
+}
--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
 
-const app = express();
-
+// Configurations and middlewares
 dotenv.config();
-const PORT = process.env.PORT || 3000;
-
-// Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }))
+const app = express();
 app.use(express.json());
+app.use(helmet);
+app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: false} ));
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
 
 // 404 Page
 app.get("*", (req, res) => {

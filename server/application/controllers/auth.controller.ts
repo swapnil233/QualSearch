9a3b86afc1e0608f9641e3e13a1dcbb81b4c9d57
Hasan Iqbal <swapniliqbal@gmail.com>
2023-01-18 15:19:43 -0500

    Server side validation checks

--- server/application/controllers/auth.controller.ts
+++ server/application/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { RefreshToken, User } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 import { hashToken } from '../../infrastructure/utils/hashToken.util';
+import { body, validationResult } from 'express-validator';
 const { v4: uuidv4 } = require('uuid');
 const { generateTokens } = require('../../infrastructure/utils/jwt.util');
 const { addRefreshTokenToWhitelist, findRefreshTokenById, deleteRefreshToken, revokeTokens } = require('../../infrastructure/services/auth.service');
@@ -12,22 +13,36 @@ export const registerUsernamePassword = async (req: Request, res: Response, next
     try {
         const { email, password, username, firstName, middleName, lastName, role } = req.body;
 
+        // Email validation check
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).send("400 error: Invalid email format.");
+        }
+
+        // Username validation check
+        if (!username || username.length < 6) {
+            return res.status(400).send("400 error: Username must be at least 6 characters.");
+        }
+
+        // Password validation check
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (!passwordRegex.test(password)) {
+            return res.status(400).send("400 error: Password must contain at least one special character and one number.");
+        }
+
         if (!email || !password || !username) {
-            res.status(400).send("400 error: You must provide an username, email, and a password to register.");
-            throw new Error('You must provide an username, email and a password.');
+            return res.status(400).send("400 error: You must provide an username, email, and a password to register.");
         }
 
         const emailExists = await findUserByEmail(email)
         const usernameExists = await findUserByUsername(username);
 
         if (emailExists) {
-            res.status(400).send("400 error: Email already in use.");
-            throw new Error('Email already in use.');
+            return res.status(400).send("400 error: Email already in use.");
         }
 
         if (usernameExists) {
-            res.status(400).send("400 error: Username already in use.");
-            throw new Error('Email already in use.');
+            return res.status(400).send("400 error: Username already in use.");
         }
 
         const user = await createUserByEmailAndPassword({ email, password, username, firstName, lastName, middleName, role });
@@ -35,33 +50,32 @@ export const registerUsernamePassword = async (req: Request, res: Response, next
         const { accessToken, refreshToken } = generateTokens(user, jti);
         await addRefreshTokenToWhitelist({ jti, refreshToken, userId: user.id });
 
-        res.json({
+        return res.json({
             accessToken,
-            refreshToken,
+            refreshToken
         });
     } catch (error) {
-        console.log(error);
-        res.status(400).send(error);
+        return res.status(400).send(error);
     }
 }
 
 export const loginUsernamePassword = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { username, password } = req.body;
-        if (!username || !password) res.status(400).send("400 error: Must provide an username and password to log in");
+        if (!username || !password) return res.status(400).send("400 error: Must provide an username and password to log in");
 
         const existingUser: User = await findUserByUsername(username);
-        if (!existingUser) res.status(403).send("403 error: Invalid login credentials.");
+        if (!existingUser) return res.status(403).send("403 error: Invalid login credentials.");
 
         const validPassword = await bcrypt.compare(password, existingUser.password);
-        if (!validPassword) res.status(403).send("403 error: Invalid password");
+        if (!validPassword) return res.status(403).send("403 error: Invalid password");
 
         // Generate access and refresh tokens and add them to user's profile
         const jti = uuidv4();
         const { accessToken, refreshToken } = generateTokens(existingUser, jti);
         await addRefreshTokenToWhitelist({ jti, refreshToken, userId: existingUser.id });
 
-        res.json({
+        return res.json({
             accessToken,
             refreshToken
         })
@@ -75,34 +89,30 @@ export const refreshToken = async (req: Request, res: Response, next: NextFuncti
     try {
         const { refreshToken }: { refreshToken: string } = req.body;
         if (!refreshToken) {
-            res.status(400);
-            throw new Error("400 error: Missing refresh token");
+            return res.status(400).send("400 error: Missing refresh token");
         }
 
         const payload = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
         const savedRefreshToken: RefreshToken = await findRefreshTokenById(payload.jti);
 
         if (!savedRefreshToken || savedRefreshToken.revoked == true) {
-            res.status(401).send("401 error: Unauthorized");
-            throw new Error("Unauthorized");
+            return res.status(401).send("401 error: Unauthorized");
         }
 
         const hashedToken = hashToken(refreshToken);
         if (hashedToken !== savedRefreshToken.hashedToken) {
-            res.status(401).send("401 error: Unauthorized");
-            throw new Error("Unauthorized");
+            return res.status(401).send("401 error: Unauthorized");
         }
 
         const user: User = await findUserById(payload.userId);
         if (!user) {
-            res.status(401).send("401 error: Unauthorized");
-            throw new Error("Unauthorized");
+            return res.status(401).send("401 error: Unauthorized");
         }
 
         await deleteRefreshToken(savedRefreshToken.id);
 
         const jti = uuidv4();
-        const { accessToken, refreshToken: newRefreshToken } = generateTokens(user, jti);
+        const { refreshToken: newRefreshToken } = generateTokens(user, jti);
         await addRefreshTokenToWhitelist({
             jti,
             refreshToken: newRefreshToken,
@@ -117,7 +127,7 @@ export const revokeRefreshToken = async (req: Request, res: Response, next: Next
     try {
         const { userId } = req.body;
         await revokeTokens(userId);
-        res.json({ message: `Tokens revoked for user with id #${userId}` });
+        return res.json({ message: `Tokens revoked for user with id #${userId}` });
     } catch (error) {
         next(error);
     }

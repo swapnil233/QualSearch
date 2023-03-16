41243281b7c56575cf9f8161d744d013d420eebb
Hasan Iqbal <swapniliqbal@gmail.com>
2023-03-16 17:12:49 -0400

    Additional teams functions

--- server/application/controllers/teams.controller.ts
+++ server/application/controllers/teams.controller.ts
@@ -1,5 +1,5 @@
 import { Response } from "express";
-import { createTeamByUserId, getAllTeamsByUserId } from "../../infrastructure/services/team.service";
+import { createTeamByUserId, getAllTeamsByUserId, addTeamMember, deleteTeamById, getTeamById, removeTeamMember, updateTeamById } from "../../infrastructure/services/team.service";
 import { IGetUserAuthInfoRequest } from '../../domain/interfaces/IGetUserAuthInfoRequest';
 
 export const createTeam = async (req: IGetUserAuthInfoRequest, res: Response) => {
@@ -31,5 +31,88 @@ export const getAllTeams = async (req: IGetUserAuthInfoRequest, res: Response) =
         return res.status(200).json({ message: `Retrieved all teams for ${req.payload.email}`, data: teams });
     } catch (error) {
         res.status(400).json({ message: "Error getting all teams", error })
+    }
+}
+
+// Get a specific team by its ID
+export const getTeam = async (req: IGetUserAuthInfoRequest, res: Response) => {
+    try {
+        const userId = req.payload.userId;
+        const { teamId } = req.params;
+
+        if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+        const team = await getTeamById(teamId, userId);
+
+        return res.status(200).json({ message: "Retrieved team details", data: team });
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+}
+
+// Update a team's details by its ID
+export const updateTeam = async (req: IGetUserAuthInfoRequest, res: Response) => {
+    try {
+        const userId = req.payload.userId;
+        const { teamId } = req.params;
+        const { name, description } = req.body;
+
+        if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+        const updatedTeam = await updateTeamById(teamId, userId, { name, description });
+
+        return res.status(200).json({ message: "Team updated successfully", data: updatedTeam });
+    } catch (error) {
+        res.status(400).json({ message: "Error updating team", error });
+    }
+}
+
+// Delete a team by its ID
+export const deleteTeam = async (req: IGetUserAuthInfoRequest, res: Response) => {
+    try {
+        const userId = req.payload.userId;
+        const { teamId } = req.params;
+
+        if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+        await deleteTeamById(teamId, userId);
+
+        return res.status(200).json({ message: "Team deleted successfully" });
+    } catch (error) {
+        res.status(400).json({ message: "Error deleting team", error });
+    }
+}
+
+// Add a user to a team
+export const addUserToTeam = async (req: IGetUserAuthInfoRequest, res: Response) => {
+    try {
+        const userId = req.payload.userId;
+        const { teamId } = req.params;
+        const { memberId } = req.body;
+
+        if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+        await addTeamMember(teamId, userId, memberId);
+
+        return res.status(200).json({ message: "User added to team successfully" });
+    } catch (error) {
+        res.status(400).json({ message: "Error adding user to team", error });
+    }
+}
+
+// Remove a user from a team
+export const removeUserFromTeam = async (req: IGetUserAuthInfoRequest, res: Response) => {
+    try {
+        const userId = req.payload.userId;
+        const { teamId } = req.params;
+        const { memberId } = req.body;
+
+        if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+        await removeTeamMember(teamId, userId, memberId);
+
+        return res.status(200).json({ message: "User removed from team successfully" });
+    } catch (error) {
+        res.status(400).json({ message: "Error removing user from team", error });
     }
 }
--- server/application/routes/teams.route.ts
+++ server/application/routes/teams.route.ts
@@ -1,9 +1,14 @@
-import { createTeam, getAllTeams } from '../controllers/teams.controller';
+import { createTeam, getAllTeams, getTeam, updateTeam, deleteTeam, addUserToTeam, removeUserFromTeam } from '../controllers/teams.controller';
 const { isAuthenticated } = require('../middlewares/isAuthenticated.middleware');
 const express = require("express");
 const router = express.Router();
 
 router.get("/", isAuthenticated, getAllTeams)
 router.post("/create", isAuthenticated, createTeam);
+router.get("/:teamId", isAuthenticated, getTeam);
+router.put("/:teamId", isAuthenticated, updateTeam);
+router.delete("/:teamId", isAuthenticated, deleteTeam);
+router.post("/:teamId/members/:memberId", isAuthenticated, addUserToTeam);
+router.delete("/:teamId/members/:memberId", isAuthenticated, removeUserFromTeam);
 
 module.exports = router;
